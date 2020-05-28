3f938f425991c66a401205e188076d2d9a19e541
thiagosprestes <thiago.santos.prestes@gmail.com>
2020-05-28 15:19:37 -0300

    Corrigido problema de não separar tags corretamente no array.

--- frontend/src/components/AddModal/index.js
+++ frontend/src/components/AddModal/index.js
@@ -36,7 +36,7 @@ export default function AddModal({ toggleModal }) {
             title,
             link,
             description,
-            tags: [tags],
+            tags: tags.split(' '),
         };
 
         try {
